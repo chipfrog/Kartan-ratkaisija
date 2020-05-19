a9d3cc487857f8bffb09936ef6f44f2bcb440e01
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2020-05-19 11:49:45 +0300

    Karttatiedoston  muuntaminen taulukoksi

--- src/main/java/shortest_path_visualizer/Main.java
+++ src/main/java/shortest_path_visualizer/Main.java
@@ -1,7 +1,14 @@
 package shortest_path_visualizer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        MapReader reader = new MapReader();
+
+        reader.createMatrix(new File ("src/main/resources/testikartta.txt"));
+        reader.printMap();
     }
 }
---
+++ src/main/java/shortest_path_visualizer/MapReader.java
@@ -0,0 +1,54 @@
+package shortest_path_visualizer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MapReader {
+    private char [][] array;
+    private Scanner scanner;
+    private int rows;
+    private int columns;
+
+    public MapReader() {
+
+    }
+
+    public void createMatrix(File file) throws FileNotFoundException {
+        initArray(file);
+        this.scanner = new Scanner(file);
+        int currentRow = 0;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            for (int i = 0; i < line.length(); i ++) {
+                array[currentRow][i] = line.charAt(i);
+
+            }
+            currentRow ++;
+        }
+    }
+
+    public void initArray(File file) throws FileNotFoundException {
+        this.scanner = new Scanner(file);
+        this.rows = 0;
+        this.columns = 0;
+
+        while (scanner.hasNextLine()) {
+            columns = scanner.nextLine().length();
+            rows ++;
+        }
+        this.array = new char[rows][columns];
+    }
+
+    public void printMap() {
+    for (int i = 0; i < rows; i ++) {
+        for (int j = 0; j < columns; j ++) {
+            System.out.print(array[i][j]);
+        }
+        System.out.println();
+    }
+    System.out.println("rows: " + rows);
+    System.out.println("columns: " + columns);
+    }
+}
